9eac3393d176be7b888d5a2456f418656f104501
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-12-12 13:18:53 -0500

    Enh: Style clean

--- config.php
+++ config.php
@@ -2,51 +2,24 @@
 
 namespace humhub\modules\adsense;
 
+use humhub\modules\adsense\Events;
+use humhub\modules\adsense\Module;
+use humhub\modules\admin\widgets\AdminMenu;
+use humhub\modules\directory\widgets\Sidebar;
+use humhub\modules\user\widgets\ProfileSidebar;
+use humhub\modules\space\widgets\Sidebar as Spacebar;
+use humhub\modules\dashboard\widgets\Sidebar as Dashbar;
+
 return [
     'id' => 'adsense',
-    'class' => 'humhub\modules\adsense\Module',
+    'class' => Module::class,
     'namespace' => 'humhub\modules\adsense',
     'events' => [
-        [
-            'class' => \humhub\modules\dashboard\widgets\Sidebar::class,
-            'event' => \humhub\modules\dashboard\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\adsense\Events',
-                'addAdFrame'
-            ]
-        ],
-        [
-            'class' => \humhub\modules\directory\widgets\Sidebar::class,
-            'event' => \humhub\modules\directory\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\adsense\Events',
-                'addAdFrame'
-            ]
-        ],
-        [
-            'class' => \humhub\modules\space\widgets\Sidebar::class,
-            'event' => \humhub\modules\space\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\adsense\Events',
-                'addAdFrame'
-            ]
-        ],
-        [
-            'class' => \humhub\modules\user\widgets\ProfileSidebar::class,
-            'event' => \humhub\modules\user\widgets\ProfileSidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\adsense\Events',
-                'addAdFrame'
-            ]
-        ],
-        [
-            'class' => \humhub\modules\admin\widgets\AdminMenu::class,
-            'event' => \humhub\modules\admin\widgets\AdminMenu::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\adsense\Events',
-                'onAdminMenuInit'
-            ]
-        ]
+        ['class' => Dashbar::class, 'event' => Dashbar::EVENT_INIT, 'callback' => [Events::class, 'addAdFrame']],
+        ['class' => Sidebar::class, 'event' => Sidebar::EVENT_INIT, 'callback' => [Events::class, 'addAdFrame']],
+        ['class' => Spacebar::class, 'event' => Spacebar::EVENT_INIT, 'callback' => [Events::class, 'addAdFrame']],
+        ['class' => ProfileSidebar::class, 'event' => ProfileSidebar::EVENT_INIT, 'callback' => [Events::class, 'addAdFrame']],
+        ['class' => AdminMenu::class, 'event' => AdminMenu::EVENT_INIT, 'callback' => [Events::class, 'onAdminMenuInit']]
     ]
 ];
 ?>
