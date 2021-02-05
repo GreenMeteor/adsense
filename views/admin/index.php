839889d2007f08186ae796e05670ae20e5cef7ca
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2021-02-05 04:52:31 -0500

    Enh: Yii 2 standards

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,26 +1,28 @@
 <?php
 
-use humhub\compat\CHtml;
-use humhub\models\Setting;
-use humhub\compat\CActiveForm;
-use humhub\modules\adsense\controllers\AdminController;
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use humhub\modules\ui\form\widgets\SortOrderField;
+
 ?>
+
 <div class="panel panel-default">
-	<div class="panel-heading"><?=Yii::t('AdsenseModule.base', '<strong>AdSense</strong>'); ?></div>
-	<div class="panel-body">
-		<?php $form = CActiveForm::begin(['id' => 'adsense-settings-form']); ?>
-			<?=$form->errorSummary($model); ?>
-			<div class="form-group">
-				<?=$form->labelEx($model, 'client'); ?>
-				<?=$form->textField($model, 'client', ['class' => 'form-control', 'readonly' => Setting::IsFixed('client', 'adsense')]); ?>
-			</div>
-			<p class="help-block"><?=Yii::t('AdsenseModule.base', 'eg: "ca-pub-XXXXXXXXX"'); ?></p>
-			<div class="form-group">
-				<?=$form->labelEx($model, 'slot'); ?>
-				<?=$form->textField($model, 'slot', ['class' => 'form-control', 'readonly' => Setting::IsFixed('slot', 'adsense')]); ?>
-			</div>
-			<?= CHtml::submitButton(Yii::t('AdsenseModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
-			<?=\humhub\widgets\DataSaved::widget(); ?>
-		<?php CActiveForm::end(); ?>
-	</div>
+
+    <div class="panel-heading"><?= \Yii::t('AdsenseModule.base', '<strong>AdSense</strong> module configuration') ?></div>
+
+    <div class="panel-body">
+
+        <?php $form = ActiveForm::begin(['id' => 'adsense-settings-form']); ?>
+        <div class="form-group">
+            <?= $form->field($model, 'client'); ?>
+            <?= $form->field($model, 'slot'); ?>
+            <?= $form->field($model, 'sort')->widget(SortOrderField::class) ?>
+        </div>
+
+        <div class="form-group">
+            <?= Html::submitButton(\Yii::t('AdsenseModule.base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+    </div>
 </div>
