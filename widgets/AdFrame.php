8e9f436aa63ece5a94d6033d4761f4be87eabc8a
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2021-02-05 04:57:15 -0500

    Enh: Implement new get methods for widget

--- widgets/AdFrame.php
+++ widgets/AdFrame.php
@@ -2,6 +2,7 @@
 
 namespace humhub\modules\adsense\widgets;
 
+use Yii;
 use humhub\components\Widget;
 
 class AdFrame extends Widget
@@ -9,8 +10,15 @@ class AdFrame extends Widget
 
     public $contentContainer;
 
+    /**
+     * @inheritdoc
+     */
     public function run()
     {
-        return $this->render('adframe', []);
+        $client = Yii::$app->getModule('adsense')->getClient() . '';
+
+        $slot = Yii::$app->getModule('adsense')->getSlot();
+
+        return $this->render('adframe', ['client' => $client, 'slot' => $slot]);
     }
 }
