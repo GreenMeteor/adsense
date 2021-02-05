67ae08d6b9b264e856f5f2ceb1814e0a550179d7
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2021-02-05 18:39:27 -0500

    Update Assets.php

--- Assets.php
+++ Assets.php
@@ -4,13 +4,9 @@ namespace humhub\modules\adsense;
 
 use Yii;
 use yii\web\AssetBundle;
-use yii\web\JqueryAsset;
 
 class Assets extends AssetBundle
 {
     public $sourcePath = '@adsense/assets';
 
-    public $depends = [
-        JqueryAsset::class
-    ];
 }
