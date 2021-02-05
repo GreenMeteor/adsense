b72a2550b818e085b9610552327a16f3dae18e70
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2021-02-05 04:58:07 -0500

    Enh: Implement new widget views

--- widgets/views/adframe.php
+++ widgets/views/adframe.php
@@ -1,37 +1,49 @@
 <?php
 
 use yii\helpers\Url;
-use humhub\models\Setting;
-use humhub\modules\adsense\Assets;
+use humhub\libs\Html;
+use humhub\widgets\PanelMenu;
+use humhub\modules\ui\view\components\View;
 
-Assets::register($this);
-?>
+/* @var $client string */
+/* @var $slot string */
+/* @var $this View */
 
-<div class="panel">
-  <div class="panel-heading">
-    <?=Yii::t('AdsenseModule.base', '<strong>Community</strong> Ad'); ?>
-  </div>
-  <div class="panel-body">
-  <?php
-  	if (!Setting::Get('client', 'adsense')) {
- ?>
- 	<p>Please set your ad client and ad slot ids in administration</p>
- <?php
-	} else {
+$this->registerJsFile('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', ['data-ad-client' => Yii::$app->getModule('adsense')->getClient(), 'async' => 'async', 'position' => View::POS_HEAD]);
 ?>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- In Post Correct -->
-	<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="<?php print Setting::Get('client', 'adsense');?>"
-     data-ad-slot="<?php print Setting::Get('slot', 'adsense');?>"
-     data-ad-format="auto"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-<?php
-	}
-?>
+<div class="panel panel-adsense panel-default" id="panel-adsense">
 
-  </div>
+    <?= PanelMenu::widget(['id' => 'panel-adsense']); ?>
+    <div class="panel-heading">
+        <?= Yii::t('AdsenseModule.base', '<strong>Community</strong> Ad'); ?>
+    </div>
+
+    <div class="panel-body">
+        <?= Html::beginTag('div') ?>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="<?= $client; ?>"
+            data-ad-slot="<?= $slot; ?>"
+            data-ad-format="auto"
+            data-ad-layout-key="-gu-3+1f-3d+2z"
+            data-full-width-responsive="true"></ins>
+        <script <?= Html::nonce() ?>>
+        $(document).ready(function(){
+            var $analyticsOff = $('.adsbygoogle:hidden');
+            var $analyticsOn = $('.adsbygoogle:visible');
+
+            $analyticsOff.each(function() {
+                $(this).remove();
+
+            });
+            $analyticsOn.each(function() {
+                (adsbygoogle = window.adsbygoogle || []).push({});
+
+            });
+
+        });
+        </script>
+        <?= Html::endTag('div') ?>
+    </div>
 </div>
