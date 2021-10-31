7b4587e14a056fa462ea288893716c6e4a0b9b12
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2021-10-30 20:01:22 -0400

    Fix: Remove deprecated Directory widget

--- config.php
+++ config.php
@@ -5,7 +5,6 @@ namespace humhub\modules\adsense;
 use humhub\modules\adsense\Events;
 use humhub\modules\adsense\Module;
 use humhub\modules\admin\widgets\AdminMenu;
-use humhub\modules\directory\widgets\Sidebar;
 use humhub\modules\user\widgets\ProfileSidebar;
 use humhub\modules\space\widgets\Sidebar as Spacebar;
 use humhub\modules\dashboard\widgets\Sidebar as Dashbar;
@@ -16,7 +15,6 @@ return [
     'namespace' => 'humhub\modules\adsense',
     'events' => [
         ['class' => Dashbar::class, 'event' => Dashbar::EVENT_INIT, 'callback' => [Events::class, 'addAdFrame']],
-        ['class' => Sidebar::class, 'event' => Sidebar::EVENT_INIT, 'callback' => [Events::class, 'addAdFrame']],
         ['class' => Spacebar::class, 'event' => Spacebar::EVENT_INIT, 'callback' => [Events::class, 'addAdFrame']],
         ['class' => ProfileSidebar::class, 'event' => ProfileSidebar::EVENT_INIT, 'callback' => [Events::class, 'addAdFrame']],
         ['class' => AdminMenu::class, 'event' => AdminMenu::EVENT_INIT, 'callback' => [Events::class, 'onAdminMenuInit']],
