fc6201f2433e7626e1df0fad8c721439df6438fc
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-11-10 02:20:45 -0500

    Update Assets.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- assets/Assets.php
+++ assets/Assets.php
@@ -2,11 +2,15 @@
 
 namespace humhub\modules\adsense\assets;
 
-use Yii;
 use yii\web\AssetBundle;
 
 class Assets extends AssetBundle
 {
     public $sourcePath = '@adsense/resources';
-
+    public $js = [
+        'js/adsense.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
 }
