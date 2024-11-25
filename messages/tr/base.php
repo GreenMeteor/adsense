884ee0ed1589f9628fcc14428f5ffdc2a2d114ec
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-11-25 14:37:34 -0500

    translations files
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- messages/tr/base.php
+++ messages/tr/base.php
@@ -1,7 +1,23 @@
 <?php
+
 return [
-    'AdSense' => 'AdSense',
-    'client' => 'AdSense veri-reklam-işlemci',
-    'slot' => 'AdSense veri-reklam-alan',
-    'save' => 'Kaydet',
+    '<b>AdBlocker Detected</b>' => '',
+    '<strong>Community</strong> Ad' => '',
+    'AdSense Settings' => '',
+    'Ads.txt file created successfully.' => '',
+    'Ads.txt file updated successfully.' => '',
+    'Client' => '',
+    'Content appended to Ads.txt.' => '',
+    'Each line must have exactly 4 parts: <domain>, <publisher_id>, <account_type>, <certification_id>.' => '',
+    'Please disable AdBlocker to view content.' => '',
+    'Please set your ad client and ad slot ids in <a href="/adsense/admin">admin</a>' => '',
+    'Slot' => '',
+    'Sort Order' => '',
+    'The account type must be either "DIRECT" or "RESELLER".' => '',
+    'The ads.txt content must be a single line.' => '',
+    'The ads.txt content must not exceed 56 characters.' => '',
+    'The certification ID must be a 16-character hexadecimal string.' => '',
+    'The domain must always be google.com.' => '',
+    'The publisher ID must start with "pub-" followed by exactly 16 digits.' => '',
+    'ads.txt Content' => '',
 ];
